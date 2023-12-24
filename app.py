547dc63a2886f45a33c4af74fa8110e9aa4d5d20
Victor Kim <victorkim@hotmail.ca>
2023-12-24 18:21:00 -0500

    add other routes for testing

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import sys
 from flask import Flask
 import pymysql
 from sshtunnel import SSHTunnelForwarder
+from flask import request
+
+from pythonping import ping
 
 app = Flask(__name__)
 
@@ -14,14 +17,62 @@ slaves_ip=[
 ]  # sys.argv[2:5]
 username='ubuntu'
 
+key='myp.pem'
+
 
 @app.route('/direct')
-def direct_hit(master_ip,query):
+def direct_hit():
+    query = request.args.get('query')
     print("master_ip:",master_ip)
     print("query:",query)
     
     # change ssh_key import
-    with SSHTunnelForwarder(master_ip, username, ssh_pkey='vockey.pem', remote_bind_address=(master_ip, 3306)) as tunnel:
+    with SSHTunnelForwarder(master_ip, username, ssh_pkey=key, remote_bind_address=(master_ip, 3306)) as tunnel:
+        conn = pymysql.connect(host=master_ip, user='root', password='root', db='sakila', port=3306, autocommit=True)
+        cursor = conn.cursor()
+        operation = query
+        cursor.execute(operation)
+        print(cursor.fetchall())
+
+    return cursor.fetchall(),200
+
+@app.route('/custom')
+def direct_hit():
+    query = request.args.get('query')
+    print("slaves:",slaves_ip)
+    print("query:",query)
+
+    latency={}
+
+    for slave in slaves_ip:
+        time=ping(slave,timeout=1)
+        latency[slave]=time.rtt_avg_ms
+
+    sorted=dict(sorted(latency.items(), key=lambda item: item[1]))
+    shortestslave=sorted[0]
+
+    print(shortestslave)
+    
+    # change ssh_key import
+    with SSHTunnelForwarder(shortestslave, username, ssh_pkey=key, remote_bind_address=(shortestslave, 3306)) as tunnel:
+        conn = pymysql.connect(host=shortestslave, user='root', password='root', db='sakila', port=3306, autocommit=True)
+        cursor = conn.cursor()
+        operation = query
+        cursor.execute(operation)
+        print(cursor.fetchall())
+
+    return cursor.fetchall(),200
+
+@app.route('/random')
+def direct_hit():
+    query = request.args.get('query')
+    print("slaves:",slaves_ip)
+    print("query:",query)
+
+    
+    
+    # change ssh_key import
+    with SSHTunnelForwarder(master_ip, username, ssh_pkey=key, remote_bind_address=(master_ip, 3306)) as tunnel:
         conn = pymysql.connect(host=master_ip, user='root', password='root', db='sakila', port=3306, autocommit=True)
         cursor = conn.cursor()
         operation = query
