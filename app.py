61c2aa338ad8c66f6996289fa75839854eb31660
Victor Kim <victorkim@hotmail.ca>
2023-12-25 19:32:37 -0500

    change name duplicate

--- app.py
+++ app.py
@@ -3,21 +3,23 @@ from flask import Flask
 import pymysql
 from sshtunnel import SSHTunnelForwarder
 from flask import request
+import random
+
 
 from pythonping import ping
 
 app = Flask(__name__)
 
 # change ips and push
-master_ip="172.31.21.78" # sys.argv[1]
+master_ip="44.203.141.219" # sys.argv[1]
 slaves_ip=[
-    "172.31.31.114",
-    "172.31.24.46",
-    "172.31.27.249"
+    "54.174.175.142",
+    "52.90.31.137",
+    "52.87.246.232"
 ]  # sys.argv[2:5]
 username='ubuntu'
 
-key='myp.pem'
+key='tmp1.pem'
 
 
 @app.route('/direct')
@@ -28,16 +30,17 @@ def direct_hit():
     
     # change ssh_key import
     with SSHTunnelForwarder(master_ip, username, ssh_pkey=key, remote_bind_address=(master_ip, 3306)) as tunnel:
-        conn = pymysql.connect(host=master_ip, user='root', password='root', db='sakila', port=3306, autocommit=True)
+        conn = pymysql.connect(host=master_ip, user='root',password='', db='sakila', port=3306, autocommit=True)
         cursor = conn.cursor()
         operation = query
         cursor.execute(operation)
         print(cursor.fetchall())
+        conn.close()
 
     return cursor.fetchall(),200
 
 @app.route('/custom')
-def direct_hit():
+def custom_hit():
     query = request.args.get('query')
     print("slaves:",slaves_ip)
     print("query:",query)
@@ -55,27 +58,31 @@ def direct_hit():
     
     # change ssh_key import
     with SSHTunnelForwarder(shortestslave, username, ssh_pkey=key, remote_bind_address=(shortestslave, 3306)) as tunnel:
-        conn = pymysql.connect(host=shortestslave, user='root', password='root', db='sakila', port=3306, autocommit=True)
+        conn = pymysql.connect(host=shortestslave, user='root', db='sakila', port=3306, autocommit=True)
         cursor = conn.cursor()
         operation = query
         cursor.execute(operation)
         print(cursor.fetchall())
+        conn.close()
 
     return cursor.fetchall(),200
 
 @app.route('/random')
-def direct_hit():
+def random_hit():
     query = request.args.get('query')
     print("slaves:",slaves_ip)
     print("query:",query)
+
+    random_ip=random.choice(slaves_ip)
     
     # change ssh_key import
-    with SSHTunnelForwarder(master_ip, username, ssh_pkey=key, remote_bind_address=(master_ip, 3306)) as tunnel:
-        conn = pymysql.connect(host=master_ip, user='root', password='root', db='sakila', port=3306, autocommit=True)
+    with SSHTunnelForwarder(random_ip, username, ssh_pkey=key, remote_bind_address=(random_ip, 3306)) as tunnel:
+        conn = pymysql.connect(host=random_ip, user='root', db='sakila', port=3306, autocommit=True)
         cursor = conn.cursor()
         operation = query
         cursor.execute(operation)
         print(cursor.fetchall())
+        conn.close()
 
     return cursor.fetchall(),200
         
